4ffd7410d43a54b80bef7078f4da97e56acdf510
Krystian Perez <perez.krystian@gmail.com>
2021-04-22 07:09:45 -0600

    docs: Adds original creation date and creator name

--- bikeshare.py
+++ bikeshare.py
@@ -1,6 +1,8 @@
 import time
 import pandas as pd
 import numpy as np
+# Originally created: April 21, 2021
+# Author: K.X. Perez
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
